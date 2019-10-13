70a5ec68739986ca3748bdd5faee4c407d22a47e
U-Sasuke <yaoqing1116@163.com>
2019-10-13 16:41:08 +0800

    【add】

---
+++ start/src/main/java/com/example/test/concurrent/BitOperationTest.java
@@ -0,0 +1,31 @@
+package com.example.test.concurrent;
+
+public class BitOperationTest {
+
+    public static void main(String[] args) {
+        int a = 13, b = 6;
+        System.out.println("   a    ：" + getBinaryStr(a));
+        System.out.println("   b    ：" + getBinaryStr(b));
+        System.out.println("  a&b   ：" + getBinaryStr(a & b));
+        System.out.println("  a|b   ：" + getBinaryStr(a | b));
+        System.out.println("   ~a   ：" + getBinaryStr(~a));
+        System.out.println("  a^b   ：" + getBinaryStr(a ^ b));
+        System.out.println("  a<<2  ：" + getBinaryStr(a << 2));
+        System.out.println("   -a   ：" + getBinaryStr(-a));
+        System.out.println("  a>>2  ：" + getBinaryStr(a >> 2));
+        System.out.println("(-a)>>2 ：" + getBinaryStr((-a) >> 2));
+        System.out.println("  a>>>2 ：" + getBinaryStr(a >>> 2));
+        System.out.println("(-a)>>>2：" + getBinaryStr((-a) >>> 2));
+    }
+
+    private static String getBinaryStr(int n) {
+        StringBuilder str = new StringBuilder(Integer.toBinaryString(n));
+        int len = str.length();
+        if (len < 32) {
+            for (int i = 0; i < 32 - len; i++) {
+                str.insert(0, "0");
+            }
+        }
+        return str.substring(0, 8) + " " + str.substring(8, 16) + " " + str.substring(16, 24) + " " + str.substring(24, 32);
+    }
+}
